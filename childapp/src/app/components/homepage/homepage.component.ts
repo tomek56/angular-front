1649db6edb6a277f2d1e66e24bbc51602eb4d0d0
tomek <tomek@geeksdeck.com>
2018-11-10 06:04:31 +0100

    up

--- childapp/src/app/components/homepage/homepage.component.ts
+++ childapp/src/app/components/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from 'src/app/services/http.service';
 import { HomePage } from 'src/app/models/homepage';
 import { Observable } from 'rxjs';
 import { Course } from 'src/app/models/course';
+import { LinkhelperService } from 'src/app/services/linkhelper.service';
 
 @Component({
   selector: 'app-homepage',
@@ -13,11 +14,19 @@ export class HomepageComponent implements OnInit {
 
   featured_course: Array<Course>;
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private helperLink: LinkhelperService) {
     this.getCourses();
    }
 
   ngOnInit() {
+
+  }
+
+  getUrl(course: Course): string {
+    // return 'https://www.imh.com.sg/uploadedImages/Common/VisitIMH1.jpg';
+    var link = this.helperLink.getFullLink(course.image_285x437);
+    console.log(link);
+    return link;
   }
 
   getCourses() {
@@ -25,7 +34,6 @@ export class HomepageComponent implements OnInit {
     this.httpService.getHomePageCourses().subscribe(data => {
       this.featured_course = data.featured_course;
     });
-
 
   }
 }
---
+++ childapp/src/app/services/linkhelper.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinkhelperService {
+
+  private host = 'http://127.0.0.1:8000';
+
+  constructor() { }
+
+  getFullLink(url): string {
+    return this.host + url;
+  }
+
+}
