e09909abb26c59b4e4b935573dbe4783c05e6d7e
tomek <tomek@geeksdeck.com>
2018-11-12 20:46:21 +0100

    dfg

--- childapp/src/app/app.module.ts
+++ childapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {CdkTableModule} from '@angular/cdk/table';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,8 +9,46 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SpyInterceptor } from './services/interceptor';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
 
-
+  import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -24,6 +62,14 @@ import { HttpModule } from '@angular/http';
     HttpClientModule,
     HttpModule,
     DashboardModule,
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
--- childapp/src/app/dashboard/dashboard.module.ts
+++ childapp/src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardSkeletonComponent } from './components/dashboard-skeleton/dashboard-skeleton.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DashboardSkeletonComponent],
+  declarations: [DashboardSkeletonComponent, NavigationComponent, FirstComponent, SecondComponent],
   bootstrap: [DashboardSkeletonComponent]
 })
 export class DashboardModule { }
