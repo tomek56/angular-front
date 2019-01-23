aef2e05cdd813d7adfffcd46d3aece6a5f71357a
tomek <tomek@geeksdeck.com>
2019-01-23 20:30:35 +0100

    update

--- childapp/src/app/dashboard/components/panel/components/courses-slider/courses-slider.component.ts
+++ childapp/src/app/dashboard/components/panel/components/courses-slider/courses-slider.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Course } from 'src/app/models/course';
 import { LinkhelperService } from 'src/app/services/linkhelper.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses-slider',
@@ -18,7 +19,7 @@ export class CoursesSliderComponent implements OnInit {
   @Input()
   progress: boolean;
 
-  constructor(private helperLink: LinkhelperService) { }
+  constructor(private helperLink: LinkhelperService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
   }
@@ -33,5 +34,35 @@ export class CoursesSliderComponent implements OnInit {
 
   goToCourse(course: Course) {
 
+  }
+
+  getCourseLessons(course: Course): number {
+    let count = 0;
+    // TODO po zrobieniu w API
+    return 2;
+    course.sections.forEach(section => {
+      section.lessons.forEach(lesson => {
+        count = count + 1;
+      });
+    });
+
+    return count;
+  }
+
+  goToProgressCourse(course: Course) {
+
+    if (course.lesson_to_continiue) {
+      this.router.navigate(['/course/', course.slug, course.lesson_to_continiue.lesson.id]);
+
+    } else {
+      this.router.navigate(['/course/', course.slug]);
+    }
+
+  }
+
+  getProgressBarWidth(course: Course): string {
+    let width = (250 * (course.progress / 100));
+
+    return width + 'px';
   }
 }
